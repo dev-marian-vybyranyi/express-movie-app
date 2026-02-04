273991315384d6c49223f5a56dad6fb09d957b5f
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-04 12:50:01 +0200

    Implement initial UI scaffolding with Bootstrap, including navigation and single movie views, and integrate Helmet for security.

--- movieFanSite/app.js
+++ movieFanSite/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 
 var app = express();
+const helmet = require('helmet');
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
