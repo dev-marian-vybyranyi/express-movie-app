29bf5695d6d2a43732aca6e0259def7e4a458006
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-04 13:19:05 +0200

    Display now playing movies from TMDb on the homepage, integrating dotenv for API key management and adjusting Helmet's Content Security Policy.

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -1,19 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const request = require('request');
+const request = require("request");
 
 const apiKey = process.env.API_KEY;
-const apiBaseUrl = 'http://api.themoviedb.org/3';
+const apiBaseUrl = "https://api.themoviedb.org/3";
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
-const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
+
+router.use((req, res, next) => {
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next();
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   request.get(nowPlayingUrl, (error, response, movieData) => {
+    console.log(movieData);
     const parsedData = JSON.parse(movieData);
-    console.log(parsedData);
-  })
-  res.render('index', { title: 'Express' });
+    res.render("index", {
+      parsedData: parsedData.results,
+    });
+  });
 });
 
 module.exports = router;
